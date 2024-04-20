678243831d04d24698f35e7d56851f6459f74693
CamiloCJ09 <camilo.campaz1@u.icesi.edu.co>
2024-04-20 10:45:24 -0500

    Feat: First commit setting up flask app

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # Initialize Flask extensions here
+
+    # Register blueprints here
+
+    @app.route('/test')
+    def test_page():
+        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+
+    return app
